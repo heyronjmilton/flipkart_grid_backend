00be48249f7f6e0882b85a56e3202bca9ab0fb1f
HEYRON <68512524+heyronjmilton@users.noreply.github.com>
2024-10-19 15:43:03 +0530

    added the inferencing models to the feed

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
-from fastapi import FastAPI, Request, WebSocket, WebSocketDisconnect
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import JSONResponse
 import base64
 import cv2
 import numpy as np
+
+import torch
+from ultralytics import YOLO
+
+from utils.image_process import resize_with_aspect_ratio
+
+
+device = torch.device("cuda")
+
+object_detection_model = YOLO("model\object_detection.pt")
+object_detection_model.info()
+
+object_detection_model = object_detection_model.to(device)
 
 app = FastAPI()
 
@@ -36,15 +48,27 @@ async def websocket_camera_feed(websocket: WebSocket):
             img_array = np.frombuffer(data, np.uint8)
             img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
 
+            resized_frame = resize_with_aspect_ratio(img, width=640)
+
+            results_object_detection = object_detection_model(resized_frame)
+            result_object_detection = results_object_detection[0]
+
+            image_with_boxes_object = result_object_detection.plot()
+
             # Display the image using OpenCV
-            cv2.imshow("Camera Feed", img)
+            cv2.imshow("Camera Feed", resized_frame)
+            cv2.imshow("Inferneced Camera Feed", image_with_boxes_object)
             cv2.waitKey(1)  # Display the image for 1 ms
 
             # Encode the image to base64 to send it back
-            _, buffer = cv2.imencode('.jpg', img)
+            _, buffer = cv2.imencode('.jpg', image_with_boxes_object)
             jpg_as_text = base64.b64encode(buffer).decode('utf-8')
             await websocket.send_text(f"data:image/jpeg;base64,{jpg_as_text}")  # Send the image back
 
     except WebSocketDisconnect:
         print("WebSocket connection closed.")
         cv2.destroyAllWindows()  # Close the preview window when the connection is closed
+
+@app.get("/")
+def home():
+    return {"message" : "The server is up and running"}
